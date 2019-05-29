6675787407537092f1d92714f75fdbbea5b86aa7
SuperJuniorYang <zhou.yang@shareworks.cn>
2019-05-29 16:48:02 +0800

    Thread测试

---
+++ java/Thread.java
@@ -0,0 +1,10 @@
+/**
+ * @author zhou.yang
+ * @ClassName Thread
+ * @date 19-5-29 下午4:46
+ */
+public class Thread {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
